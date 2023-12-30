e350cab71f26f841bb3be128d47f806ad4ae5abf
probeshnath <probeshn@gmail.com>
2023-12-30 21:38:39 +0600

    vite and chakra ui setup

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import {Container, Stack, Flex, List, ListItem} from '@chakra-ui/react'
+
+const App = () => {
+  return (
+    <Container maxW="800px">
+      <Stack>
+        <List>
+          <ListItem>Player</ListItem>
+        </List>
+      </Stack>
+    </Container>
+  )
+}
+
+export default App
