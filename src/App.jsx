48c97925589ebdc9feb6c48a887bb232f778e666
probeshnath <probeshn@gmail.com>
2023-12-30 21:48:08 +0600

    simple layout made

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,25 @@
 import React from 'react'
-import {Container, Stack, Flex, List, ListItem} from '@chakra-ui/react'
+import { Container, Stack, Flex, List, ListItem,Heading } from '@chakra-ui/react'
+
 
 const App = () => {
+  
   return (
     <Container maxW="800px">
-      <Stack>
-        <List>
-          <ListItem>Player</ListItem>
-        </List>
-      </Stack>
+      <Flex justify="space-between" height="90vh" align="center">
+        <Stack w="300px">
+          <Heading fontSize="3xl" color="yellow.800" textAlign="center" >Player</Heading>
+          <List p="4" minH="70vh" boxShadow="xl" borderRadius="md">
+            <ListItem>Player</ListItem>
+          </List>
+        </Stack>
+        <Stack w="300px">
+        <Heading fontSize="3xl" color="yellow.800" textAlign="center" >Main Section</Heading>
+          <List p="4" minH="70vh" boxShadow="xl" borderRadius="md">
+            <ListItem>Player</ListItem>
+          </List>
+        </Stack>
+      </Flex>
     </Container>
   )
 }
